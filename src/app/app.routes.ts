897adc1079cd52435e529551137de1a27ff21dcf
Sarah <sarahelhefnawy0@gmail.com>
2025-09-18 19:10:49 +0300

    check syntax

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { authGuard } from './core/guards/auth-guard';
 import { loggedInGuard } from './core/guards/logged-in-guard';
 import { OrderChild } from './features/order-child/order-child';
 import { OrderUserChild } from './features/order-user-child/order-user-child';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./features/home/home').then(c => c.Home), title: 'Home' },
     { path: 'products', loadComponent: () => import('./features/products/products').then(c => c.Products), title: 'Products' },
     {
         path: 'product-details/:id',
-        data: { renderMode: 'server' },
+        data: { renderMode: RenderMode.Server },
         loadComponent: () => import('./features/product-details/product-details').then(c => c.ProductDetails),
         title: 'Product Details'
     },
@@ -24,7 +25,7 @@ export const routes: Routes = [
     },
     {
         path: 'checkOut/:id',
-        data: { renderMode: 'server' },
+        data: { renderMode: RenderMode.Server },
         loadComponent: () => import('./features/check-out/check-out').then(c => c.CheckOut),
         title: 'Payment',
         canActivate: [authGuard]
